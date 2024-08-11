b01be5ec321ce97b3ede33c2af26021ed0c38a9a
ArtTeam <ArtemQ98>
2024-08-11 11:51:27 +0300

    'Update'

--- OOP_DZ.cpp
+++ OOP_DZ.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <string>
 #include <vector>
+#include <tuple>
 
 using namespace std;
 
@@ -25,22 +26,21 @@ enum room_type
 
 class sectionClass 
 {
-    
+    struct MyStruct;
     friend void Start(sectionClass* value);
 
 protected:
-    
     int square;
     building_type buildType;
     int quantityBuildings;
     vector<building_type> buildingsOnSection;
     
 public:
-
+    
    
 
     class houseClass
-    {
+    { 
         friend sectionClass;
         vector<vector<string> > rooms;
         int square;
@@ -48,15 +48,18 @@ public:
         int quantityRooms;
 
     public:
-        void StartHouse()
+        houseClass StartHouse()
         {
+            
+            houseClass User_house;
+            
             cout << "Площадь: ";
-            cin >> square;
+            cin >> User_house.square;
             while (true)
             {
                 cout << "Количестко этажей: ";
-                cin >> quantityFloors;
-                if (quantityFloors < 1 || quantityFloors > 3)
+                cin >> User_house.quantityFloors;
+                if (User_house.quantityFloors < 1 || User_house.quantityFloors > 3)
                 {
                     cout << "Неверное значение" << endl;
                     continue;
@@ -67,15 +70,15 @@ public:
                 }
             }
             
-            rooms.resize(quantityFloors);
+            User_house.rooms.resize(User_house.quantityFloors);
             string room;
-            for (int i = 0; i < quantityFloors; i++)
+            for (int i = 0; i < User_house.quantityFloors; i++)
             {
                 while (true)
                 {
                     cout << "Сколько комнат на " << i + 1 << " этаже? ";
-                    cin >> quantityRooms;
-                    if (quantityRooms < 2 || quantityRooms > 4)
+                    cin >> User_house.quantityRooms;
+                    if (User_house.quantityRooms < 2 || User_house.quantityRooms > 4)
                     {
                         cout << "Неверное значение" << endl;
                         continue;
@@ -85,37 +88,53 @@ public:
                         break;
                     }
                 }
-                
-                rooms[i].resize(quantityRooms);
-                for (int j = 0; j < quantityRooms; j++)
+
+                User_house.rooms[i].resize(User_house.quantityRooms);
+                for (int j = 0; j < User_house.quantityRooms; j++)
                 {
                     cout << "Комната " << j + 1 << ": ";
                     cin >> room;
-                    rooms[i][j] = room;
+                    User_house.rooms[i][j] = room;
                 }
             }
+            return User_house;
         }
+        
         void PrintHouse()
         {
-            cout << "Площадь дома: " << quantityRooms << endl;
-            cout << "Этажей: " << quantityFloors<< endl;
-            for (int i = 0; i < quantityFloors; i++)
+           
+            cout << "Площадь дома: " << this->square << "м^2" << endl;
+            cout << "Этажей в доме: " << this->quantityFloors << endl;
+            for (int i = 0; i < this->quantityFloors; i++)
             {
-                cout << "Комнаты на " << i+1 << " этаже: ";
-                for (int j = 0; j < size(rooms[i]); j++)
+                cout << "Комнаты на " << i + 1 << " этаже: ";
+                for (int j = 0; j < size(this->rooms[i]); j++)
                 {
-                    if (size(rooms[i]) - 1 == j)
+                    if (size(this->rooms[i]) - 1 == j)
                     {
-                        cout << rooms[i][j];
+                        cout << this->rooms[i][j];
                     }
                     else
                     {
-                        cout << rooms[i][j] << ", ";
+                        cout << this->rooms[i][j] << ", ";
                     }
                 }
                 cout << endl;
             }
+            
+           
         }
+
+        int GetSquare()
+        {
+            return this->square;
+        }
+
+    };
+
+    class garageClass
+    {
+        int square;
     };
 
     void Print()
@@ -125,22 +144,22 @@ public:
             if (buildingsOnSection[i] == building_type::Дом)
             {
                 cout << "--------Дом--------" << endl;
-                this->User_house.PrintHouse();
+                
+                houses[i].PrintHouse();
             }
         }
     }
 
-
+    
 public:
-    houseClass User_house;
+    sectionClass::houseClass* temp = new sectionClass::houseClass;
+    garageClass User_garage;
+    vector<houseClass> houses;
+    friend houseClass;
+
 };
 
 
-
-class garageClass
-{
-    int square;
-};
 
 class shedClass
 {
@@ -171,7 +190,7 @@ void Start(sectionClass *value)
             if (build == "Дом")
             {
                 value->buildType = building_type::Дом;
-                value->User_house.StartHouse();
+                value->houses.push_back(value->temp->StartHouse()); 
                 value->buildingsOnSection.push_back(building_type::Дом);
                 break;
             }
@@ -193,6 +212,7 @@ int main()
 
     vector<sectionClass> section;
     int quantitySectios;
+    int sum{0};
     cout << "Кол-во учатсков: ";
     cin >> quantitySectios;
     cin.ignore();
@@ -203,8 +223,10 @@ int main()
         cout << "========УЧАСТОК" << i + 1 << "========" << endl;
         Start(sectioni);
         section.push_back(*sectioni);
+        delete sectioni;
     }
-
+    
+    cout << sum << endl;
     cout << endl << endl << endl << "========Вывод========" << endl << endl << endl;
 
     for (int i = 0; i < size(section); i++)
@@ -212,6 +234,14 @@ int main()
         
         cout << "========УЧАСТОК" << i+1 << "========" << endl;
         section[i].Print();
+
+        cout << endl << endl << endl << "========Площадь========" << endl << endl << endl;
+       
+        for (int j = 0; j < size(section[i].houses); j++)
+        {
+            sum += section[i].houses[j].GetSquare();
+        }
+        cout << sum << endl;
     }
 }
 
